f45dca5da2519cd620258aaf5f8592f19d8e46b8
hri123 <hrishikesh@hrishikeshs-mbp.in.ibm.com>
2016-05-18 14:43:33 +0530

    NA

--- DSA/source/JavaScript/NativeScript.js
+++ DSA/source/JavaScript/NativeScript.js
@@ -11,9 +11,17 @@ npm install -g nativescript
 tns create hello-world
 cd hello-world
 tns platform add ios
-tns prepare ios (ignore errors during this step, without this step the bundle identifier is not generated, this step is required when source is updated)
-(fix issues in the identity portal in the Xcode editor)
+tns prepare ios (without this step the bundle identifier is not generated, this step is required when source is updated too)
+(do this if you are getting error during tns prepare - sudo gem install xcodeproj (if throwing error - cannot load such file -- xcodeproj (LoadError)))
+Now open in xcode and follow steps in the below links.
+(fix issues in the identity portal in the Xcode editor, also make sure the name in the Bundle Identifier is correct)
 (on phone -> settings -> general -> device management -> trust)
+
+<h4>How to run an app on iphone running ios without developer account:</h4>
+<ul>
+<li><a href='https://www.youtube.com/watch?v=NxvJOf71e0w'>youtube video</a></li>
+<li><a href='http://www.idownloadblog.com/2015/09/18/how-to-compile-apps-using-xcode-7-to-run-on-a-non-jailbroken-device/'>blog</a></li>
+</ul>
 
 
 */
