0d667dbd0c152ed5b257c1022ee21ad0ae28b9f7
hri123 <hrishikesh@hrishikeshs-mbp.in.ibm.com>
2016-05-18 14:15:42 +0530

    Nativescript on iphone

--- DSA/source/JavaScript/NativeScript.js
+++ DSA/source/JavaScript/NativeScript.js
@@ -5,6 +5,17 @@
 
 <a href="https://www.nativescript.org/" target="_blank">Read this for more details about NativeScript</a>
 
+<h4> code in NativeScript, deploy on your iphone </h4>
+
+npm install -g nativescript
+tns create hello-world
+cd hello-world
+tns platform add ios
+tns prepare ios (ignore errors during this step, without this step the bundle identifier is not generated, this step is required when source is updated)
+(fix issues in the identity portal in the Xcode editor)
+(on phone -> settings -> general -> device management -> trust)
+
+
 */
 
 })();
