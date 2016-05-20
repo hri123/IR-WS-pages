b1234ca35779fa60eee82580bcbe713791213e32
hri123 <hrishikesh@Hrishikeshs-MacBook-Pro.local>
2016-05-21 00:34:05 +0530

    NA

---
+++ DSA/source/JavaScript/css_tips_tricks.js
@@ -0,0 +1,27 @@
+(function() {
+
+
+/*
+
+Bootstrap
+Less
+
+
+<h4>Interview Questions</h4>
+
+css BOX model
+css inline vs block elements
+diff between visibility:hidden vs display:none
+
+< div>
+  < div>
+  < /div>
+  < div> <-- set the width, height, background
+  < /div>
+< /div>
+
+change the text color of a tab menu label of a dojo tab container
+
+create a table (three column) like structure with css
+
+})();
