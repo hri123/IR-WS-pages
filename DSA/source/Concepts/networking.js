d99bc14920599df474be0c1af18dfe2f187c1dbb
hri123 <hrishikesh@Hrishikeshs-MacBook-Pro.local>
2016-05-20 12:08:17 +0530

    NA

---
+++ DSA/source/Concepts/networking.js
@@ -0,0 +1,35 @@
+(function() {
+
+
+/*
+
+
+subnet -
+
+
+http://superuser.com/questions/158291/whats-the-meaning-of-10-0-0-1-24-address-of-my-computer-ip-addr-command
+Subnet - is a range of IP addresses determined by part of an address (often called the network address) and a subnet mask (netmask). For example, if the netmask is 255.255.255.0 (or /24 for short), and the network address is 192.168.10.0, then that defines a range of IP addresses 192.168.10.0 through 192.168.10.255. Shorthand for writing that is 192.168.10.0/24. (http://superuser.com/questions/353664/what-is-the-difference-between-a-vlan-and-a-subnet)
+
+switch -
+
+A network switch (sometimes known as a switching hub) is acomputer networking device that is used to connect devices together on a computer network, by using a form of packet switching to forward data to the destination device.
+(my guess - switch is connecting computers in the same subnet, different subnets, you need a router in addition to switch ?)
+
+
+VLAN -
+
+switch partitioning
+
+Router -
+
+Let's say a computer with an interface with an IP address of 192.168.10.4/24 wants to talk to a computer with an interface with an IP address of 192.168.20.4/24. Different subnets, so we must go through a router.
+
+Gateway -
+
+A "gateway" always means the IP address of a router.
+
+firewall - 
+
+*/
+
+})();
