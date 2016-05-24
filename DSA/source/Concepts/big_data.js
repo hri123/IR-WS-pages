6f9a95a94acd3200f4e158f88c6986812d098470
hri123 <hrishikesh@hrishikeshs-mbp.in.ibm.com>
2016-05-24 17:23:47 +0530

    NA

---
+++ DSA/source/Concepts/big_data.js
@@ -0,0 +1,17 @@
+(function() {
+
+
+/*
+
+<a href="https://www.udemy.com/big-data-basics-hadoop-mapreduce-hive-pig-spark/learn/#/lecture/3517218:0" target="_blank">Big Data Basics</a>
+
+<img border="0" src="source/Concepts/images/BigData/CourseOutline.png" />
+<img border="0" src="source/Concepts/images/BigData/WhatIsHadoop.png" />
+<img border="0" src="source/Concepts/images/BigData/WhatIsMapReduce.png" />
+<img border="0" src="source/Concepts/images/BigData/MapReduceSample.png" />
+<img border="0" src="source/Concepts/images/BigData/Hive.png" />
+<img border="0" src="source/Concepts/images/BigData/ApachePig.png" />
+
+*/
+
+})();
