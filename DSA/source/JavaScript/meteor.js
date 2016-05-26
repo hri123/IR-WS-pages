1c87e330bfff4539db95486bc0d8e6bbeeb081e0
hri123 <hri123@gmail.com>
2016-05-26 11:59:42 +0530

    NA

--- DSA/source/JavaScript/meteor.js
+++ DSA/source/JavaScript/meteor.js
@@ -3,8 +3,15 @@
 
 /*
 
+https://www.meteor.com/install
+https://www.meteor.com/try
 
+meteor add iron:router
+meteor update iron:router
 
+Copy the Meteor_Seed_Project from Inspinia
+cd into it
+meteor
 
 */
 
