707a47ea9df2e4eb015a202b501bb508eb36cd7a
hri123 <hri123@gmail.com>
2016-05-29 18:54:43 +0530

    NA

--- DSA/source/JavaScript/LinkedList/ListNode.js
+++ DSA/source/JavaScript/LinkedList/ListNode.js
@@ -1,8 +1,36 @@
 var exports = module.exports = {};
 
-exports.ListNode = function () {
+exports.ListNode = function (in_data) {
     
+    var data;
+    var next;
     
+    data = in_data;
     
+    return {
+        setData: function(in_data) {
+            data = in_data;
+        },
+        
+        getData: function () {
+            return data;
+        },
+        
+        setNext: function (in_next) {
+            next = in_next;
+        },
+        
+        getNext: function() {
+            return next;
+        }
+    }
     
-}
+};
+
+// Usage:
+
+// var moduleListNode = require('./ListNode.js');
+
+// var obj = new moduleListNode.ListNode(10);
+
+// console.log(obj.getData());
