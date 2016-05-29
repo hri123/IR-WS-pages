a309dbb8f757ed2ea10392e0cf4877239adc4d2f
hri123 <hri123@gmail.com>
2016-05-29 20:57:56 +0530

    NA

---
+++ DSA/source/JavaScript/angular.js
@@ -0,0 +1,80 @@
+(function () {
+    
+/*
+
+<a href="http://www.informit.com/articles/article.aspx?p=2271482&seqNum=3">An Overview of the AngularJS Life Cycle</a>
+
+<table class="table table-bordered table-striped">
+<thead>
+<tr>
+<th>Concept</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#template">Template</a></td>
+<td>HTML with additional markup</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#directive">Directives</a></td>
+<td>extend HTML with custom attributes and elements</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#model">Model</a></td>
+<td>the data shown to the user in the view and with which the user interacts</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#scope">Scope</a></td>
+<td>context where the model is stored so that controllers, directives and expressions can access it</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#expression">Expressions</a></td>
+<td>access variables and functions from the scope</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#compiler">Compiler</a></td>
+<td>parses the template and instantiates directives and expressions</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#filter">Filter</a></td>
+<td>formats the value of an expression for display to the user</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#view">View</a></td>
+<td>what the user sees (the DOM)</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#databinding">Data Binding</a></td>
+<td>sync data between the model and the view</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#controller">Controller</a></td>
+<td>the business logic behind views</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#di">Dependency Injection</a></td>
+<td>Creates and wires objects and functions</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#injector">Injector</a></td>
+<td>dependency injection container</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#module">Module</a></td>
+<td>a container for the different parts of an app including controllers, services, filters, directives which configures the Injector</td>
+</tr>
+<tr>
+<td><a href="https://docs.angularjs.org/guide/concepts#service">Service</a></td>
+<td>reusable business logic independent of views</td>
+</tr>
+</tbody>
+</table>
+
+
+
+*/ 
+
+   
+    
+})();
