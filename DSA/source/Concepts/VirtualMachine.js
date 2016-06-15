b309675af63dcb148d47ba2d34892a2481ee313c
hri123 <hri123@gmail.com>
2016-06-15 13:37:50 +0530

    NA

---
+++ DSA/source/Concepts/VirtualMachine.js
@@ -0,0 +1,15 @@
+(function () {
+
+/*
+
+<h4>How to destroy Vagrant VMs if 'vagrant destroy' command does not work</h4>
+
+ps axu|grep VBoxHeadless
+kill -TERM 31867
+
+then
+vagrant destroy
+
+*/
+
+})();
